da492661ae0646356449c9079364ad00999c078d
Mikhail Vasyak <miahasik@gmail.com>
2026-06-23 07:11:17 +0900

    Реализован адаптив, добавлены JS-функции и подключены favicon

--- js/main.js
+++ js/main.js
@@ -2,3 +2,24 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+function callback() {
+  let number = prompt('Введите ваш номер телефона');
+  alert('Ожидайте звонок в течение 15 минут.');
+}
+
+// let password = prompt('Введите пароль:');
+// if (password == 'mshqqSecretKey') {
+//   document.body.style.display = 'block';
+// } else {
+//   alert('Ошибка авторизации: неправильный пароль');
+// }
+
+function selectCategory(id) {
+  catBtn = document.getElementById(id);
+  alert(catBtn.classList.includes('active'));
+}
+
+function subscribe() {
+  alert('Вы успешно подписались на рассылку!');
+}
